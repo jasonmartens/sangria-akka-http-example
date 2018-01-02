28c8f85ebb5f8b4ed014a5b72b9b3c9c3f1d0c2f
Jason Martens <jmartens@zendesk.com>
2018-01-02 10:58:38 -0800

    Refactoring and cleanup

--- src/main/scala/SchemaDefinition.scala
+++ src/main/scala/SchemaDefinition.scala
@@ -1,6 +1,10 @@
 import java.util.UUID
 
-import sangria.ast.{AstVisitor, ObjectTypeDefinition, Argument => AstArgument, Document => AstDocument, Field => AstField, FieldDefinition => AstFieldDefinition, OperationDefinition => AstOperationDefinition, OperationType => AstOperationType, Selection => AstSelection, StringValue => AstStringValue, TypeDefinition => AstTypeDefinition, Value => AstValue, ObjectValue => AstObjectValue, ObjectField => AstObjectField, ListValue => AstListValue, NameValue => AstNameValue, ScalarValue => AstScalarValue}
+import sangria.ast.{AstVisitor, ObjectTypeDefinition, Argument => AstArgument, Document => AstDocument, Field => AstField}
+import sangria.ast.{FieldDefinition => AstFieldDefinition, OperationDefinition => AstOperationDefinition}
+import sangria.ast.{OperationType => AstOperationType, Selection => AstSelection, StringValue => AstStringValue}
+import sangria.ast.{TypeDefinition => AstTypeDefinition, Value => AstValue, ObjectValue => AstObjectValue}
+import sangria.ast.{ObjectField => AstObjectField, ListValue => AstListValue, NameValue => AstNameValue, ScalarValue => AstScalarValue}
 import sangria.parser.QueryParser
 import sangria.schema._
 import sangria.validation.QueryValidator
@@ -85,7 +89,7 @@ object SchemaDefinition {
         Map(astField.name -> obj(astField.name))
       case ScalarType("String", _, _, _, _, _, _, _) =>
         Map(astField.name -> obj(astField.name))
-      case ListType(objType@ObjectType(objName, _, _, _, _, _)) =>
+      case ListType(objType @ ObjectType(_, _, _, _, _, _)) =>
         val objectIDs = obj(astField.name).asInstanceOf[Vector[String]]
         val objects = objectIDs.map { id =>
           resolveObject(id, objType, astField.selections)
@@ -136,7 +140,6 @@ object SchemaDefinition {
 
   def createObject(objValue: AstObjectValue, outputType: OutputType[_], inputType: InputType[_], selections: Vector[AstSelection]): Option[Map[String, Any]] = {
     val id = UUID.randomUUID().toString
-    val objType = outputType
     val objTypeName = outputType match { case OptionType(ObjectType(objName, _, _, _, _, _)) => objName}
     val newObject: Map[String, Any] = objValue.fields.foldLeft(Map[String, Any]("id"-> id))((coll, field) => coll ++ Map(field.name -> resolveInputField(field.value)))
     data((id, objTypeName)) = newObject
@@ -146,42 +149,46 @@ object SchemaDefinition {
     Some(returnObject)
   }
 
+  def executeQuery(schema: Schema[_, _], field: AstField): Map[String, Any] = {
+    val outputType = schema.query.fields.find(schemaField => schemaField.name == field.name).get.fieldType
+    resolveField(field, outputType, Map.empty)
+  }
+
+  def executeMutation(schema: Schema[_, _], fieldName: String, argName: String, argValue: AstValue, selections: Vector[AstSelection]): Map[String, Any] = {
+    val outputType = schema.mutation.map(m => m.fieldsByName(fieldName).head.fieldType).get
+    val inputTypes: InputType[_] = schema.mutation
+      .flatMap(
+        m => m.fieldsByName(fieldName).head
+          .arguments.find(_.name == argName)
+          .map { case Argument(_, it, _, _, _, _) => it }
+      ).get
+    argValue match {
+      case objValue: AstObjectValue =>
+        val obj = createObject(objValue, outputType, inputTypes, selections)
+        val arg = AstArgument("id", AstStringValue(obj.get("id").asInstanceOf[String]))
+        val f = AstField(None, fieldName, Vector(arg), Vector.empty, selections)
+        resolveField(f, outputType, Map.empty)
+      case _ =>
+        ???
+    }
+  }
+
   def execute(qAst: AstDocument): JsValue = {
     import spray.json._
     import DefaultJsonProtocol._
-    var count = 0
 
     val violations = QueryValidator.default.validateQuery(schema, qAst)
     if (violations.nonEmpty) {
       throw new RuntimeException(s"Query failed validation: $violations")
     }
 
-    val values = AstVisitor.visitAstWithState(schema, qAst, mutable.Map.empty[String, Any]) { (typeInfo, state) ⇒
-      AstVisitor.simple(onLeave = {
-        case AstOperationDefinition(AstOperationType.Query, _, _, _, Vector(f @ AstField(_, _, _, _, _, _, _, _)), _, _, _) =>
-          val outputType = schema.query.fields.find(schemaField => schemaField.name == f.name).get.fieldType
-          state("data") = resolveField(f, outputType, Map.empty)
-        case AstOperationDefinition(AstOperationType.Mutation, _, _, _, Vector(AstField(_, fieldName, Vector(AstArgument(argName, value, _, _)), _, selections, _, _, _)), _, _, _) =>
-          val outputType = schema.mutation.map(m => m.fieldsByName(fieldName).head.fieldType).get
-          val inputTypes: InputType[_] =  schema.mutation
-            .flatMap(m => m.fieldsByName(fieldName).head
-              .arguments.find(_.name == argName)
-              .map{ case Argument(_, it, _, _, _, _) => it}).get
-          value match {
-            case objValue: AstObjectValue =>
-              val obj = createObject(objValue, outputType, inputTypes, selections)
-              val arg = AstArgument("id", AstStringValue(obj.get("id").asInstanceOf[String]))
-              val f = AstField(None, fieldName, Vector(arg), Vector.empty, selections)
-              state("data") = resolveField(f, outputType, Map.empty)
-            case notImplemented =>
-              ???
-          }
-          count += 1
-        case x =>
-          println(s"$typeInfo $state, $x")
-      })
+    val result = qAst match {
+      case AstDocument(Vector(AstOperationDefinition(AstOperationType.Query, _, _, _, Vector(field @ AstField(_, _, _, _, _, _, _, _)), _, _, _)), _, _, _) =>
+        Map("data" -> executeQuery(schema, field))
+      case AstDocument(Vector(AstOperationDefinition(AstOperationType.Mutation, _, _, _, Vector(AstField(_, fieldName, Vector(AstArgument(argName, argValue, _, _)), _, selections, _, _, _)), _, _, _)), _, _, _) =>
+        Map("data" -> executeMutation(schema, fieldName, argName, argValue, selections))
     }
-    println(count)
-    values.toMap.toJson
+
+    result.toJson
   }
 }
